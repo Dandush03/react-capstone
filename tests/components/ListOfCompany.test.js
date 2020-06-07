8abf26f9dfb148b2a912d24d92ed85dbc79e2a39
Daniel Laloush <d.laloush@outlook.com>
2020-06-06 20:19:49 -0400

    Fix Jest Error

--- tests/components/ListOfCompany.test.js
+++ tests/components/ListOfCompany.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
 
 import storeConfig from '../../src/store';
 import ListOfCompany from '../../src/components/ListOfCompany';
@@ -9,7 +10,7 @@ const store = storeConfig();
 
 describe('components', () => {
   describe('ListOfCompany', () => {
-    const component = renderer.create(<ListOfCompany store={store} />);
+    const component = renderer.create(<Provider store={store}><ListOfCompany /></Provider>);
     const tree = component.toJSON();
     it('Should match to Snapshot', () => {
       expect(tree).toMatchSnapshot();
